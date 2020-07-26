6add9ca34f9ff10090d1683a285df3cbed275977
MyeongchanJin <jmcchan@naver.com>
2020-07-26 23:35:26 +0900

    Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Factoral 피연산자 입력 -> ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n + "! = " + Factorial(n));
+        }
+        
+        static int Factorial(int n)
+        {
+            if (n == 1) return 1;
+            return n * Factorial(--n);
+        }
+
+    }
+}
