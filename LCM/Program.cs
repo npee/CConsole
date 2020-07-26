1d643a6690cef121121644109f08817d62d1a942
MyeongchanJin <jmcchan@naver.com>
2020-07-26 23:35:41 +0900

    LCM

---
+++ LCM/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("첫 번째 수: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("두 번째 수: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("최소공배수: " + lcm(x, y));
+        }
+
+        public static int lcm(int x, int y)
+        {
+            if (x >= y)
+                return x * y / gcd(x, y);
+            else
+                return x * y / gcd(y, x);
+        }
+
+        public static int gcd(int x, int y)
+        {
+            if (y == 0) return x;
+            int r = x % y;
+            x = y;
+            y = r;
+            return gcd(x, y);
+        }
+    }
+}
