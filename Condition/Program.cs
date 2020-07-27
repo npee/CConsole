bd1a6468fd68b6d25e9b4c1b8f2b4e8f4e54708e
MyeongchanJin <jmcchan@naver.com>
2020-07-27 23:04:35 +0900

    삼항연산자

---
+++ Condition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("영문자를 입력해주세요 : ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input.Any(char.IsUpper) ?  "Yes" : "No");
+        }
+    }
+}
